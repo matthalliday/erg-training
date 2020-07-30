737b688e85a0b5e0c1169d34b65f33b300cb4a6d
Matt Halliday <matthalliday@hey.com>
2020-07-29 22:09:33 -0400

    Install and configure Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  devise_for :users,
+    path: 'account',
+    path_names: {
+      sign_in: 'sign-in',
+      sign_out: 'log-out',
+      sign_up: 'sign-up'
+    }
 end
